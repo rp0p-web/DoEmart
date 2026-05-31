8df1fe779f173c8b984274f5e5ca035640d9e5f8
rp0p-web <rp0pbus@gmail.com>
2026-05-31 10:30:35 +0530

    images1

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,10 +10,11 @@ app.use(express.json());
 
 /* DB CONNECTION */
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Rp0p",
-  database: "dummy"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
